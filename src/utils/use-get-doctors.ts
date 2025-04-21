9b6504e4dd0e2c68a2e3ab79a2cc758485073925
codeinshort <70271642+sureshvakude@users.noreply.github.com>
2025-04-21 23:39:22 +0530

    Update use-get-doctors.ts

--- src/utils/use-get-doctors.ts
+++ src/utils/use-get-doctors.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://127.0.0.1:8000/api/doctors/';
+const API_URL = 'https://actysuresh.pythonanywhere.com/api/doctors/';
 
 const getDoctors = async () => {
   try {
